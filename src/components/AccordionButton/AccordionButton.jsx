82e4ba7e7d5b4fb79b5b3a5f2f1178603d345cbd
Abdullayev Bahodir <bahodira213@gmail.com>
2023-05-11 04:59:04 +0500

    acc button as sep comp

---
+++ src/components/AccordionButton/AccordionButton.jsx
@@ -0,0 +1,20 @@
+import classes from "./AccordionButton.module.scss";
+export const AccordionButton = ({open, onClickHandler}) => {
+  return (
+    <button
+      className={`${classes.accordionButton} ${
+        open && classes.accordionButtonActive
+      }`}
+      onClick={onClickHandler}
+    >
+      <img
+        src={
+          !open
+            ? `/svgs/chevron-down.svg`
+            : `/svgs/chevron-down-active.svg`
+        }
+        alt="down"
+      />
+    </button>
+  );
+};
