ea8dca9e640adfc92012403b894c0e55f08b4c94
Abdullayev Bahodir <bahodira213@gmail.com>
2023-05-10 18:25:55 +0500

    jsx & css of folder and file. basic

--- src/components/FileComponent/FileComponent.jsx
+++ src/components/FileComponent/FileComponent.jsx
@@ -1,12 +1,46 @@
-import classes from './FileComponent.module.scss';
+import { useState } from "react";
+import classes from "./FileComponent.module.scss";
+import { ClickAwayListener } from "../ClickAwayListener";
 
-export const FileComponent = ({id,name, index}) => {
-    return (
-        <div className={classes.content}>
-            <div>ID: {id}</div>
-            <div>Name: {name}</div>
-            <div>Index: {index}</div>
-            <div>Type: file</div>
+export const FileComponent = ({ id, name, index, upperIndexesArray = [] }) => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <div className={classes.content}>
+      <div className={classes.innerContent}>
+        <div>
+          <div className={classes.label}>№</div>
+          <div className={classes.value}>
+            {[...upperIndexesArray, index].join(".")}
+          </div>
         </div>
-    )
-}
+        <div>
+          <div className={classes.label}>Название</div>
+          <div className={`${classes.value} ${classes.name}`}>
+            <img src={"/svgs/file.svg"} alt="file" />
+            <span>{name}</span>
+          </div>
+        </div>
+        <div>
+          <div className={classes.label}>Очередность</div>
+          <div className={classes.value}>{index}</div>
+        </div>
+        <div className={classes.settingsBox}>
+          <ClickAwayListener onClickAway={() => setShowMenu(false)}>
+            <button
+              className={`${classes.menuButton} ${
+                showMenu && classes.activeMenBut
+              }`}
+              onClick={() => setShowMenu(!showMenu)}
+            >
+              <img
+                src={!showMenu ? `/svgs/dots.svg` : `/svgs/dots-active.svg`}
+                alt="down"
+              />
+            </button>
+          </ClickAwayListener>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/FolderComponent/FolderComponent.jsx
+++ src/components/FolderComponent/FolderComponent.jsx
@@ -1,32 +1,87 @@
-import { useMemo } from 'react';
-import { renderComponent } from '../../utils';
+import { useMemo, useState } from "react";
+
+import { renderComponent } from "../../utils";
 import classes from "./FolderComponent.module.scss";
-export const FolderComponent = ({id, name, innerContent, index}) => {
-    const innerContentNames=useMemo(()=>innerContent?.map(item=>item.name).join(', '),[innerContent]);
-    return (
-        <div className={classes.content}>
-            <div className={classes.cell}>
-                <div className={classes.label}>№</div>
-                <div className={classes.value}>{index}</div>
-            </div>
-            <div className={classes.cell}>
-                <div className={classes.label}>Название</div>
-                <div className={`${classes.value} ${classes.name}`}>{name}</div>
-            </div>
-            <div className={classes.cell}>
-                <div className={classes.label}>Очередность</div>
-                <div className={classes.index}>{index}</div>
-            </div>
-            <div className={classes.cell}>
-                <div className={classes.label}>Подкатегории</div>
-                <div className={classes.index}>{innerContentNames}</div>
-            </div>
-            <div className={classes.settingsBox}>
-                <div>{innerContent.length}</div>
-            </div>
-            <div>
-                {innerContent?.map((item, index) => renderComponent(item, index))}
-            </div>
+import { ClickAwayListener } from "../ClickAwayListener";
+
+export const FolderComponent = ({
+  id,
+  name,
+  innerContent,
+  index,
+  upperIndexesArray = [],
+}) => {
+  const innerContentNames = useMemo(
+    () => innerContent?.map((item) => item.name).join(", "),
+    [innerContent]
+  );
+
+  const [showInnerItems, setShowInnerItems] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <div className={classes.content}>
+      <div className={classes.innerContent}>
+        <div>
+          <div className={classes.label}>№</div>
+          <div className={classes.value}>
+            {[...upperIndexesArray, index].join(".")}
+          </div>
         </div>
-    )
-}
+        <div>
+          <div className={classes.label}>Название</div>
+          <div className={`${classes.value} ${classes.name}`}>
+            <img src={"/svgs/folder.svg"} alt="folder" />
+            <span>{name}</span>
+          </div>
+        </div>
+        <div>
+          <div className={classes.label}>Очередность</div>
+          <div className={classes.value}>{index}</div>
+        </div>
+        <div>
+          <div className={classes.label}>Подкатегории</div>
+          <div className={classes.value}>{innerContentNames}</div>
+        </div>
+        <div className={classes.settingsBox}>
+          <div className={classes.innerItemsNumber}>{innerContent.length}</div>
+          <button
+            className={`${classes.accordionButton} ${
+              showInnerItems && classes.activeAcBut
+            }`}
+            onClick={() => setShowInnerItems(!showInnerItems)}
+          >
+            <img
+              src={
+                !showInnerItems
+                  ? `/svgs/chevron-down.svg`
+                  : `/svgs/chevron-down-active.svg`
+              }
+              alt="down"
+            />
+          </button>
+          <ClickAwayListener onClickAway={() => setShowMenu(false)}>
+            <button
+              className={`${classes.menuButton} ${
+                showMenu && classes.activeMenBut
+              }`}
+              onClick={() => setShowMenu(!showMenu)}
+            >
+              <img
+                src={!showMenu ? `/svgs/dots.svg` : `/svgs/dots-active.svg`}
+                alt="down"
+              />
+            </button>
+          </ClickAwayListener>
+        </div>
+      </div>
+      {showInnerItems && (
+        <div>
+          {innerContent?.map((item, i) =>
+            renderComponent(item, i + 1, [...upperIndexesArray, index])
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
